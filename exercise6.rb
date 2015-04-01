e97ee236920cbf99de435f4ce4d79148caf74cb6
Jordan <jordanmcrae@hotmail.com>
2015-04-01 18:22:03 -0400

    Completed assignment.

--- exercise6.rb
+++ exercise6.rb
@@ -7,7 +7,7 @@
 
 # 2. You realize you've forgotten some rice, add it to your list and output it again. Given that you've already output your list twice, it might be good to consider writing a method to do this. Putting common bits of code in a method lets you reuse it throughout your program.
 
-grocery_list = ["milk", "eggs", "peanut butter", "bread"]
+grocery_list = ["milk", "eggs", "peanut butter", "bread", "salmon"]
 
 def add_asterisk(list_name)
   list_name.each { |item| puts "* " + item }
@@ -29,4 +29,20 @@ if grocery_list.include?("bananas")
   else
     puts "You need to pick up bananas today."
 end
+
+
+# 5. Display the second item in the list. (Don't forget that arrays are zero-indexed!)
+
+puts "The second item in your list is #{grocery_list[1]}."
+
+# 6. It turns out that everything in this grocery store you're visiting is stored alphabetically, so to better plan out what you need to buy, you should sort your grocery list and output it with asterisks again.
+
+add_asterisk(grocery_list.sort)
+
+# 7. After looking everywhere, you can't find the salmon. Delete it from your list and redisplay it.
+grocery_list.delete("salmon")
+
+puts "Since you could not find salmon, it has been removed from your list. Your updated list is:"
+add_asterisk(grocery_list)
+
 
