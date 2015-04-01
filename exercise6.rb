1cba7e6f4c649f089a3f27923621f634dcfe0c0b
Jordan <jordanmcrae@hotmail.com>
2015-04-01 17:18:43 -0400

    Grocery list assignment. Step one, completed.

---
+++ exercise6.rb
@@ -0,0 +1,11 @@
+# We're going to make a shopping list by storing a few items in an array. Feel free to start with whatever items you like.
+# 1. Your next step should present your grocery list with an item on each line, with an asterisk (*) in front of it
+grocery_list = ["milk", "eggs", "peanut butter", "bread"]
+
+grocery_list.each do |asterisk|
+  puts "* " + asterisk
+end
+
+
+
+
